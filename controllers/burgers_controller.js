b6720d5bc8655f8bb085bdefeb1b95eed3e3147e
Rodney R <majid.muhassan@gmail.com>
2017-12-14 01:52:43 +0000

    working on the burgers_controller and setting up the router.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,17 @@
+//importing express and burger.js
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+//create the 'router' for the app and export the 'router' at the end of the file.
+// this portion goes out and listens for requests and retrieves the data
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+//this portion creates. Post = create
