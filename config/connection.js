5320382f34fcdbe3e64a6ce02e27494bf182c512
Rodney R <majid.muhassan@gmail.com>
2017-12-15 20:18:00 +0000

    JAWS added

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,29 @@
 // source = https://codeforgeek.com/2015/01/nodejs-mysql-tutorial/
 // and the sienfield exercise from class
-// I still need to figure out JAW for heroku
+// I still need to figure out JAW for hero
 var express = require('express')
 var mysql = require('mysql');
 var app = express();
 var port = 8080;
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: "root",
-    password: "",
-    database: 'burger_db'
+    host: "tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "mjrxwd0bl48itzpt",
+    password: "aq9g351xc9ynteh6",
+    database: 'qre0rvfue853tsee'
 });
+
+//when deploying locally use below.
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: "root",
+//     password: "",
+//     database: 'burger_db'
+// });
+
+
+
+
+
 //gang initiation connect yourself fool!
 connection.connect(function(err) {
     if (!err) {
